7be85f3d00b57056765c12639a36a3d77a090c8c
Elian ORIOU <elian.oriou@4sh.fr>
2026-03-08 15:51:06 +0100

    updated content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://walien.tech',
 	integrations: [mdx(), sitemap()],
 });
